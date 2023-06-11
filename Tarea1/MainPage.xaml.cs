924ab1330bd094d63785802b2620e127599a005d
maplicano <maplicano@DESKTOP-CFG8MP5>
2023-06-10 21:08:07 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Suma(object sender, EventArgs e)
+        {
+            var n1 = double.Parse(numero1.Text);
+            var n2 = double.Parse(numero2.Text);
+            double suma = n1 + n2;
+
+          await Navigation.PushAsync(new Views.PageTwo(suma));
+        }
+
+        private async void Resta(object sender, EventArgs e)
+        {
+            var n1 = double.Parse(numero1.Text);
+            var n2 = double.Parse(numero2.Text);
+            var resta = n1 - n2;
+
+            await Navigation.PushAsync(new Views.PageTwo(resta));
+        }
+
+        private async void Multi(object sender, EventArgs e)
+        {
+            var n1 = double.Parse(numero1.Text);
+            var n2 = double.Parse(numero2.Text);
+            var multi = n1 * n2;
+
+            await Navigation.PushAsync(new Views.PageTwo(multi));
+        }
+
+        private async void Divi(object sender, EventArgs e)
+        {
+            var n1 = double.Parse(numero1.Text);
+            var n2 = double.Parse(numero2.Text);
+
+            if (n2 != 0)
+            {
+                var div = n1 / n2;
+                await Navigation.PushAsync(new Views.PageTwo(div));
+            }
+            else
+            {
+                Resultado.Text = "No se puede realizar la división";
+            }
+        }
+
+        //fin de funciones
+    }
+}
---
+++ Tarea1/Views/PageTwo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageTwo : ContentPage
+    {
+        public PageTwo()
+        {
+            InitializeComponent();
+        }
+        public PageTwo(double suma)
+        {
+            InitializeComponent();
+
+            lbResultado.Text = "El resultado es: " + suma;
+        }
+    }
+}
